f1f8abbe391e7f02695d75556d53beeb28b34971
Bibek <bbekdml2020@gmail.com>
2023-04-04 03:11:08 +0545

    Ready for deployment

--- medkit_record_management_system/settings.py
+++ medkit_record_management_system/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,7 +125,6 @@ STATICFILES_DIRS = [
     BASE_DIR, 'statics'
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
