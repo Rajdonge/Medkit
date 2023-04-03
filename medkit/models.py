0225a46be0fc17918e8d0e8329b90c485fadb8be
Bibek <bbekdml2020@gmail.com>
2023-04-04 02:45:09 +0545

    done vercel setting

--- medkit/models.py
+++ medkit/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.utils import timezone
 
 # Create your models here.
 
@@ -29,20 +27,3 @@ class Sale_Information(models.Model):
         error_messages={'invalid': 'Please enter numbers!'})
     sale_id = models.ForeignKey(Information, on_delete=models.CASCADE)
 
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    published_date = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self) -> str:
-        return self.title
-    
-class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
-    created_date = models.DateField(default=timezone.now)
-
-    def __str__(self) -> str:
-        return self.content
